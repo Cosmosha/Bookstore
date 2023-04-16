570ed9734a9f1cf7541de5819e2fa78ee6774f37
cosmos hagan <cosmoshagan@cosmoss-MacBook-Pro.local>
2023-04-16 09:55:14 +0000

    Update Booklist components

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import { React } from 'react';
 import BookList from './BookList';
 
 const Books = () => (
-  <div>
+  <>
     <BookList />
-  </div>
+    {' '}
+  </>
 );
 
 export default Books;
