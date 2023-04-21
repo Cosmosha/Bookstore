01d1e7240091761314823843922d27b7edbc97f3
cosmos hagan <cosmoshagan@cosmoss-MacBook-Pro.local>
2023-04-21 01:29:13 +0000

    Update store config

--- src/index.js
+++ src/index.js
@@ -10,11 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
+      <Provider store={stre}>
         <App />
-      </Provider>
-      {' '}
-    </BrowserRouter>
-    {' '}
-  </React.StrictMode>,
+      </Provider>{' '}
+    </BrowserRouter>{' '}
+  </React.StrictMode>
 );
